fedc2849a8359d7a28160c9eea63d2a2a86ab97a
Md. Abu Towsif <towsif1528@gmail.com>
2024-12-22 23:19:45 +0600

    Update delete_user.php

--- Final_Lab_Task_1/model/delete_user.php
+++ Final_Lab_Task_1/model/delete_user.php
@@ -2,9 +2,8 @@
 $idd = $_REQUEST['id'];
 $idt = $_REQUEST['idt'];
 require_once("usermodel.php");
-$conn = getConnection();
-$sql = "DELETE FROM users WHERE id='$idt'";
-$result = mysqli_query($conn, $sql);
+
+$result = delete_user($idt);
 if($result){
     echo "user deleted";
 }
@@ -13,5 +12,5 @@ else{
 }
 ?>
 
-<a href="../view/view_users.php?id=<?php echo $idd   ?>"> Go Back</a>
+<a href="../view/view_users.php?id=<?php echo $idd ?>"> Go Back</a>
 
